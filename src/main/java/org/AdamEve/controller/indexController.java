a6c67d618525ec10995c7365959ad10a1efcc44d
cwennendy <cwennendy@cs.stonybrook.edu>
2018-04-27 14:39:41 -0400

    Setup

---
+++ src/main/java/org/AdamEve/controller/indexController.java
@@ -0,0 +1,18 @@
+package org.AdamEve.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class indexController {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String home(Model model)
+	{
+		return "home";
+	}
+}
