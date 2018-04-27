ddfed0af3cf446f4ea53217191bd776b6dff536a
Spencer X <spencerbinxia@gmail.com>
2018-04-27 13:30:16 -0400

    Initial commit

---
+++ src/test/java/org/AdamEve/AdamMeetsEve/AdamMeetsEveApplicationTests.java
@@ -0,0 +1,16 @@
+package org.AdamEve.AdamMeetsEve;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdamMeetsEveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
