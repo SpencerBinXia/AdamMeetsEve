c5d1e0458d874ff67269c717f0be226f3a2a056a
cwennendy <cwennendy@cs.stonybrook.edu>
2018-04-29 18:55:47 -0400

    profile controller

---
+++ src/main/java/org/AdamEve/controller/profileController.java
@@ -0,0 +1,27 @@
+package org.AdamEve.controller;
+
+import org.AdamEve.object.user;
+import org.AdamEve.service.userService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/viewuser/{ssn}/{profileID}")
+public class profileController {
+	
+	@Autowired
+	private userService userService;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String displayProfile(@PathVariable("ssn") String userid,@PathVariable("profileID") String profileName, Model model)
+	{
+		user foundUser = userService.findUser(userid);
+		model.addAttribute("foundUser", foundUser);
+		return "profile";
+	}
+	
+}
